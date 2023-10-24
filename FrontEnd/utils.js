662fc27a252ada8c31c841a4e564ec5ff7f723cb
Manuel Caro <manuelcaro321@hotmail.com>
2023-10-24 12:35:53 -0500

    create react native app todolist

---
+++ FrontEnd/utils.js
@@ -0,0 +1,68 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import dotenv from 'dotenv';
+import logNode from 'log-node';
+import log from 'log';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export const root = __dirname;
+
+/** @param {string[]} paths */
+export function joinWithRoot(...paths) {
+  return path.normalize(path.join(root, ...paths));
+}
+
+/**
+ * @template T, P
+ * @param {(...args: P) => T} callback
+ * @param {P} args
+ * @return {() => T}
+ */
+export function onceCallback(callback, ...args) {
+  if (typeof callback !== 'function') throw TypeError('parameter callback must be type function');
+  let value;
+  let onceCall = false;
+  return () => {
+    if (!onceCall) {
+      value = callback(...args);
+      onceCall = true;
+    }
+    return value;
+  }
+}
+
+export const dotenvPathApp = joinWithRoot('../.env');
+export const dotenvPath = joinWithRoot('.env');
+
+export const useDotenv = onceCallback(() => {
+  dotenv.config({ path: dotenvPathApp });
+  dotenv.config({
+    path: dotenvPath,
+    override: !(process.env['DOTENV_APP']?.toLowerCase() === 'true')
+  });
+})
+
+export function createLogger() {
+  process.env['LOG_LEVEL'] = process.env['DEBUG'] ? 'debug' : 'info';
+  const level = process.env['LOG_LEVEL'];
+  const writter = logNode();
+  return {
+    level,
+    writter,
+    log
+  }
+}
+
+export const useLogger = onceCallback(createLogger);
+
+export default {
+  root,
+  joinWithRoot,
+  onceCallback,
+  dotenvPathApp,
+  dotenvPath,
+  useDotenv,
+  createLogger,
+  useLogger
+}
