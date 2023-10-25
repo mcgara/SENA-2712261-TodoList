334af8c01e1b018ff6c6fc1618a80135b04818ec
Manuel Caro <manuelcaro321@hotmail.com>
2023-10-25 11:54:07 -0500

    create frontend app and test with api backend

---
+++ FrontEnd/index.js
@@ -0,0 +1,67 @@
+const { spawn } = require('node:child_process');
+const { useDotenv, onceCallback, useLogger } = require('./utils.node.js');
+
+const commandRunFrontEnd = 'npx expo start';
+
+/** @type {import('node:child_process').SpawnOptions} */
+const runFrontEndSpawnOptions = {
+  stdio: 'inherit',
+  shell: true
+}
+
+const prefixExpoEnv = 'EXPO_PUBLIC';
+
+const useFronEndEnv = onceCallback(() => {
+  useDotenv();
+  
+  const frontend = {
+    host: process.env['FE_HOST'],
+    port: process.env['FE_PORT'],
+    https: process.env['FE_HTTPS'],
+    localhost: process.env['FE_LOCALHOST']
+  }
+  
+  if (frontend.host) process.env['EXPO_PACKAGER_PROXY_URL'] = frontend.host;
+  const argv = process.argv.slice(2);
+  if (frontend.port) argv.push('--port', frontend.port);
+  if (frontend.https === 'true') argv.push('--https');
+  if (frontend.localhost === 'true') argv.push('--localhost');
+
+  return argv;
+})
+
+const useBackEndEnv = onceCallback(() => {
+  useDotenv();
+
+  const backend = {
+    BE_HOST: process.env['BE_HOST'],
+    BE_PORT: process.env['BE_PORT']
+  }
+  
+  Object.keys(backend).forEach(key => {
+    if (!backend[key]) return;
+    process.env[prefixExpoEnv + '_' + key] = backend[key];
+  });
+})
+
+function runFrontEnd() {
+  const logger = useLogger();
+  
+  useBackEndEnv();
+  const argv = [
+    ...process.argv.slice(2),
+    ...useFronEndEnv()
+  ];
+
+  const app = spawn(commandRunFrontEnd, argv, runFrontEndSpawnOptions);
+  app.on('error', err => logger.error('FrontEnd %s:' + err.message, err.name));
+}
+
+module.exports = {
+  commandRunFrontEnd,
+  runFrontEndSpawnOptions,
+  prefixExpoEnv,
+  useFronEndEnv,
+  useBackEndEnv,
+  runFrontEnd
+}
--- FrontEnd/utils.js
+++ FrontEnd/utils.js
@@ -1,25 +1,10 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import dotenv from 'dotenv';
-import logNode from 'log-node';
-import log from 'log';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-export const root = __dirname;
-
-/** @param {string[]} paths */
-export function joinWithRoot(...paths) {
-  return path.normalize(path.join(root, ...paths));
-}
-
 /**
  * @template T, P
  * @param {(...args: P) => T} callback
  * @param {P} args
  * @return {() => T}
  */
-export function onceCallback(callback, ...args) {
+function onceCallback(callback, ...args) {
   if (typeof callback !== 'function') throw TypeError('parameter callback must be type function');
   let value;
   let onceCall = false;
@@ -32,37 +17,6 @@ export function onceCallback(callback, ...args) {
   }
 }
 
-export const dotenvPathApp = joinWithRoot('../.env');
-export const dotenvPath = joinWithRoot('.env');
-
-export const useDotenv = onceCallback(() => {
-  dotenv.config({ path: dotenvPathApp });
-  dotenv.config({
-    path: dotenvPath,
-    override: !(process.env['DOTENV_APP']?.toLowerCase() === 'true')
-  });
-})
-
-export function createLogger() {
-  process.env['LOG_LEVEL'] = process.env['DEBUG'] ? 'debug' : 'info';
-  const level = process.env['LOG_LEVEL'];
-  const writter = logNode();
-  return {
-    level,
-    writter,
-    log
-  }
-}
-
-export const useLogger = onceCallback(createLogger);
-
-export default {
-  root,
-  joinWithRoot,
-  onceCallback,
-  dotenvPathApp,
-  dotenvPath,
-  useDotenv,
-  createLogger,
-  useLogger
+module.exports = {
+  onceCallback
 }
