021e0a420982bf3432c5465b07597097e0468db5
Manuel Caro <manuelcaro321@hotmail.com>
2023-10-25 16:35:41 -0500

    add script main of app

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Modular projects
+import { spawn } from 'node:child_process';
+
+const commands = {
+  database: 'npm run database',
+  backend: 'npm run backend',
+  frontend: 'npm run frontend',
+}
+
+export function runApp() {
+  // const database = spawn(commands.database);
+  const backend = spawn(commands.backend, { stdio: 'inherit', shell: true });
+  const frontend = spawn(commands.frontend, { stdio: 'inherit', shell: true });
+  
+  backend.on('error', err => console.error(err));
+  frontend.on('error', err => console.error(err));
+}
+
+export default {
+  runApp
+}
