bb81cb9a625aa87064f052f98ddd8ec5245cf5c9
Manuel Caro <manuelcaro321@hotmail.com>
2023-10-25 16:33:56 -0500

    add run frontend modular

--- FrontEnd/components/Main.jsx
+++ FrontEnd/components/Main.jsx
@@ -6,9 +6,6 @@ export function Main() {
     <View>
       <Text>Hello World App TodoList</Text>
       <User id={1}/>
-      <Text>BE_ENV {process.env['EXPO_PUBLIC_BE_HOST']}</Text>
-      <Text>EXPO_PUBLIC_HELLO {process.env['EXPO_PUBLIC_HELLO']}</Text>
-      <Text>EXPO_PUBLIC_HELLO2 {process.env['EXPO_PUBLIC_HELLO2']}</Text>
     </View>
   )
 }
