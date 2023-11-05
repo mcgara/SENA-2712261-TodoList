e164e99e12a32feba705f9813afdea0f79afb5f5
Manuel Caro <manuelcaro321@hotmail.com>
2023-11-05 16:04:06 -0500

    fix for in to for of and catch invalid url

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -13,20 +13,20 @@ const frontEndSpawnOptions = {
 }
 
 const defaultConfig = {
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 8020,
   https: false,
-  url: 'http://localhost:8020',
+  url: 'http://127.0.0.1:8020',
   localhost: false,
   offline: false,
   debug: false
 }
 
-const defaultConfigAPI = {
-  API_HOST: 'localhost',
+const defaultConfigApi = {
+  API_HOST: '127.0.0.1',
   API_PORT: 8010,
   API_HTTPS: false,
-  API_URL: 'http://localhost:8010'
+  API_URL: 'http://127.0.0.1:8010'
 }
 
 const useFrontEndConfig = onceCallback(() => {
@@ -43,8 +43,8 @@ const useFrontEndConfig = onceCallback(() => {
   }
 
   const config = { ...defaultConfig }
-  const realEnviron = Object.entries(environ).filter(item => !!item[0]);
-  for (const [key, value] in realEnviron) config[key] = value;
+  const realEnviron = Object.entries(environ).filter(item => !!item[1]);
+  for (const [key, value] of realEnviron) config[key] = value;
   
   config.https = String(config.https) === 'true';
   config.localhost = String(config.localhost) === 'true';
@@ -53,7 +53,9 @@ const useFrontEndConfig = onceCallback(() => {
   config.port = Number(config.port);
   if (isNaN(config.port)) config.port = defaultConfig.port;
   
-  const url = new URL(config.url);
+  let url = null
+  try { url = new URL(config.url); }
+  catch { url = new URL(defaultConfig.url); }
   url.hostname = config.host;
   url.port = String(config.port);
   url.protocol = config.https ? 'https' : 'http';
@@ -72,15 +74,17 @@ const useApiConfig = onceCallback(() => {
     API_URL: process.env['BE_URL']
   }
 
-  const config = { ...defaultConfigAPI }
-  const realEnviron = Object.entries(environ).filter(item => !!item[0]);
-  for (const [key, value] in realEnviron) config[key] = value;
+  const config = { ...defaultConfigApi }
+  const realEnviron = Object.entries(environ).filter(item => !!item[1]);
+  for (const [key, value] of realEnviron) config[key] = value;
 
   config.API_PORT = Number(config.API_PORT);
   if (isNaN(config.API_PORT)) config.API_PORT = defaultConfig.API_PORT;
   config.API_HTTPS = String(config.API_HTTPS) === 'true';
 
-  const url = new URL(config.API_URL);
+  let url = null
+  try { url = new URL(config.API_URL); }
+  catch { url = new URL(defaultConfigApi.API_URL); }
   url.hostname = config.API_HOST;
   url.port = String(config.API_PORT);
   url.protocol = config.API_HTTPS ? 'https' : 'http';
@@ -114,8 +118,8 @@ const useFrontEnd = onceCallback(() => {
   if (!isExpoURL) process.env[ExpoURLEnv] = config.url;
 
   // Set Api config
-  for (const [ENV_API, ENV_API_VALUE] in Object.entries(configApi)) {
-    const ENV_EXPO_API = `${ExpoPrefixEnv}_${ENV_API}`
+  for (const [ENV_API, ENV_API_VALUE] of Object.entries(configApi)) {
+    const ENV_EXPO_API = `${ExpoPrefixEnv}_${ENV_API}`;
     process.env[ENV_EXPO_API] = ENV_API_VALUE;
   }
 
@@ -130,7 +134,7 @@ module.exports = {
   ExpoPrefixEnv,
   ExpoURLEnv,
   defaultConfig,
-  defaultConfigAPI,
+  defaultConfigApi,
   useFrontEndConfig,
   useFrontEndArgv,
   useApiConfig,
